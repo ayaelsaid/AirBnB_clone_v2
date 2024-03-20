bd4a00d096bcaf9ed083b7abc41c0e835b1d68ad
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-03-20 04:12:09 +0200

    Update console.py

--- console.py
+++ console.py
@@ -117,10 +117,8 @@ class HBNBCommand(cmd.Cmd):
         """ Create an object of any class"""
         if not args:
             print("** class name missing **")
-            return
         elif args not in HBNBCommand.classes:
             print("** class doesn't exist **")
-            return
         new_instance = HBNBCommand.classes[args]()
         storage.save()
         print(new_instance.id)
