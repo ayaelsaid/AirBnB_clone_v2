38318a0a4ad7f63f60daaff563f47748c1a57e6f
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-03-21 01:29:52 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -52,7 +52,7 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-        if '_sa_instance_state' in dictionary[key]
+        if '_sa_instance_state' in dictionary.key():
             del dictionary['_sa_instance_state']
         return dictionary
 
