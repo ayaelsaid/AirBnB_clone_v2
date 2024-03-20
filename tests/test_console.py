8e4f4e5c946769a005fccbd3abdf6185c8260991
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-03-20 03:42:49 +0200

    Update test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -8,26 +8,28 @@ import inspect
 import pep8
 import unittest
 
-class Testconsole(unittest.TestCase):
-  def test_empty_args(self):
-    result = HBNBCommand().do_create("")
-    self.assertEqual(result, "** class name missing **")
-  
-  def test_class_not_in_classes(self):
-    result = HBNBCommand().do_create("Bat")
-    self.assertEqual(result, "** class doesn't exist **")
-  
-  def test_do_dic(self):
-    result = HBNBCommand().do_create('User name="California" id=4 grad=5.5')
-    self.assertIsInstance(result, str)
 
-     def test_datatype_value(self):
+class Testconsole(unittest.TestCase):
+ 
+    def test_empty_args(self):
+        result = HBNBCommand().do_create("")
+        self.assertEqual(result, "** class name missing **")
+
+    def test_class_not_in_classes(self):
+        result = HBNBCommand().do_create("Bat")
+        self.assertEqual(result, "** class doesn't exist **")
+
+    def test_do_dic(self):
+        result = HBNBCommand().do_create('User name="California" id=4 grad=5.5')
+        self.assertIsInstance(result, str)
+
+    def test_datatype_value(self):
         result_str = HBNBCommand().do_create('User name="John" age=30')
         self.assertIsInstance(result, User)
 
         result_int = HBNBCommand().do_create('User name="John" age=30')
         self.assertIsInstance(result, User)
-        
+
         result_float = HBNBCommand().do_create('User name="John" age=30.5')
         self.assertIsInstance(result, User)
 
