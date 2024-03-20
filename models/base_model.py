9247960a0bd95358c20e75c341e7a45590ae4fe3
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-03-20 20:04:59 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -17,9 +17,9 @@ class BaseModel:
         else:
             self.id = kwargs.get('id', str(uuid.uuid4()))
             self.created_at = datetime.strptime(kwargs.get('created_at'),
-                                                           '%Y-%m-%dT%H:%M:%S.%f')
+                                                '%Y-%m-%dT%H:%M:%S.%f')
             self.updated_at = datetime.strptime(kwargs.get('updated_at'),
-                                                           '%Y-%m-%dT%H:%M:%S.%f')
+                                                '%Y-%m-%dT%H:%M:%S.%f')
             del kwargs['__class__']
             self.__dict__.update(kwargs)
 
