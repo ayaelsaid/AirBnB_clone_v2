e647aa84f45ad26f7a6e426e53196491a36dcb94
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-03-21 04:46:35 +0200

    Update city.py

--- models/city.py
+++ models/city.py
@@ -8,6 +8,6 @@ from sqlalchemy.orm import relationship
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
-    state_id = column(string(60), nullable=False, ForeignKey('states.id'))
-    name = column(String(128), nullable=False)
+    state_id = Column(string(60), nullable=False, ForeignKey('states.id'))
+    name = Column(String(128), nullable=False)
     places = relationship('Place', cascade='all, delete', backref='cities')
