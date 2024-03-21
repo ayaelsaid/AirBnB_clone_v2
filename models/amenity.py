55e4f06cfa7a4834245cd5917eadbfe99c579d36
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-03-21 04:10:50 +0200

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Table, ForeignKey
+from sqlalchemy.orm import relationship
 
+association_table = Table('place_amenity', Base.metadata,
+                          Column('place_id', String(60), ForeignKey('places.id')),
+                          Column('amenity_id', String(60), ForeignKey('amenities.id'))
+                          )
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """ Amenity class, represents amenities table """
+    __tablename__ = 'amenities'
+
+    name = Column(String(128), nullable=False)
