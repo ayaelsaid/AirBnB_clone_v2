c0b525cde512669dc10720ca8b102a2c96ce39cf
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-03-21 02:06:35 +0200

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+import os
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        mycities = relationship("City", cascade="all, delete-orphan",
+                                backref="state")
+    else:
+        
+        def mycities(self):
+            """ method for cities """
+            from models import storage
+            from models.city import City
+            new_list = []
+            cities_dict = storage.all(City)
+            for city in cities_dict.values():
+                if city.state_id == self.id:
+                    new_list.append(city)
+            return new_list
